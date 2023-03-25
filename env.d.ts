42850349faef8edc9021be8569b69a9db2e0bf0f
Dario Jiménez Ayala <jeffrey.ayala2040@gmail.com>
2023-03-25 18:32:04 -0400

    feat: initial commit

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        NEXT_PUBLIC_API_URL: string;
+        NEXT_PUBLIC_TIMEOUT: string;
+      }
+    }
+  }
+  
+  export {};
+  
